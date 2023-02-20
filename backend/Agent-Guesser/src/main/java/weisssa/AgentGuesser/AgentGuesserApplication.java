4720e3e358a06d4447b7728b1d6c104fad25d0bc
WeissSa <WeissBsamuel@gmail.com>
2023-02-19 20:21:12 -0500

    added spring boot template

---
+++ backend/Agent-Guesser/src/main/java/weisssa/AgentGuesser/AgentGuesserApplication.java
@@ -0,0 +1,13 @@
+package weisssa.AgentGuesser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentGuesserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentGuesserApplication.class, args);
+	}
+
+}
---
+++ backend/Agent-Guesser/src/test/java/weisssa/AgentGuesser/AgentGuesserApplicationTests.java
@@ -0,0 +1,13 @@
+package weisssa.AgentGuesser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentGuesserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
